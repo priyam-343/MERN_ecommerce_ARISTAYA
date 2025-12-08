ac77759623c7463aec52d734f80a8c2b0a72e4fc
Priyam Kumar <priyam3451@gmail.com>
2025-12-09 04:28:10 +0530

    commit

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -284,7 +284,7 @@ const paymentVerification = async (req, res) => {
           }
 
           const transport = nodemailer.createTransport({
-            service: "gmail", host: "smtp.gmail.email", port: 465,
+            service: "gmail", host: "smtp.gmail.com", port: 465,
             auth: { user: process.env.EMAIL, pass: process.env.EMAIL_PASSWORD },
           })
 
